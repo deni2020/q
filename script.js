285a8a20d65b9cb82d9c84132ab99742c390508b
deni2020 <62760422+deni2020@users.noreply.github.com>
2020-08-07 21:26:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-!function(q) {
-	if(!q) throw alert(`usage: ${location.href.replace(/#|$/,'#')}QUERY`)
+console.log(123)
+// alert(123)
+const se = {
+	vivaldi(q,data) {
+		"use strict";
+
+		let firstSpaceIndex = q.indexOf(' ');
+		if (firstSpaceIndex===-1) firstSpaceIndex=q.length;
+
+		let keyword=q.substr(0,firstSpaceIndex);
+		q=q.substr(firstSpaceIndex+1);
+
+		let search = data.engines.filter(a=>a.keyword===keyword);
+		if (1<search.length) throw {message:'more then 1 search keyword matched',search,keyword};
+		else  if(search.length!==1) return 'not found';
+		search=search[0];
+
+
+	    console.log(1);
+		location.replace(search.url.replace(/%S/gis,q));
+	},
+	duckduckgoBangs(q,data){
+		throw {message:'TODO!'};
+	},
+	duckduckgo(q,data){
+	    console.log(2);
+		location.replace('https://duckduckgo.com/?q='+encodeURIComponent(q));
+	}
+};
+void async function(){
+	"use strict";
+	try{
+		let q = decodeURIComponent(location.hash.substr(1));
+		if(!q) throw {message:`usage: ${location.href.replace(/#|$/,'#')}QUERY (v2)`};
+
+		let data = {
+			vivaldi: await(await fetch('vivaldi_x-search-engine.json')).json()
+		};
+
+		for (let i of ['vivaldi','duckduckgo']) se[i](q,data[i]);
+
+
+	} catch (e) {window.console.error(e);window.alert(e.message||'Error ocured, check console')}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}(location.hash.substr(1))
+}()
