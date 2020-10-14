41a5c9da06f0d6f14062dc66ce38c30add55e3de
denisde4ev <40498286+denisde4ev@users.noreply.github.com>
2020-10-14 11:35:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
-console.log(123)
-// alert(123)
-const se = {
-	vivaldi(q,data) {
-		"use strict";
-
-		let firstSpaceIndex = q.indexOf(' ');
-		if (firstSpaceIndex===-1) firstSpaceIndex=q.length;
-
-		let keyword=q.substr(0,firstSpaceIndex);
-		q=q.substr(firstSpaceIndex+1);
+const {location,console,alert} = window;
+const searchFnObj = {
+	async vivaldi(q,data) {
+		'use strict';
+		data=data||await(await fetch('vivaldi_x-search-engine.json')).json();
+		var [,keyword,q]=q.match(/([^ ]+) ?(.+)?/);
 
 		let search = data.engines.filter(a=>a.keyword===keyword);
 		if (1<search.length) throw {message:'more then 1 search keyword matched',search,keyword};
-		else  if(search.length!==1) return 'not found';
+		else if(search.length!==1) return 'not found';
 		search=search[0];
 
 
 		console.log(1);
 		location.replace(search.url.replace(/%S/gis,q));
-		return true
+		return true;
 	},
 	duckduckgoBangs(q,data){
 		throw {message:'TODO!'};
 	},
 	duckduckgo(q,data){
-	    console.log(2);
+		'use strict';
+		
+		console.log(2);
 		location.replace('https://duckduckgo.com/?q='+encodeURIComponent(q));
-		return true
+		return true;
 	}
 };
 void async function(){
-	"use strict";
+	'use strict';
+	
 	try{
 		let q = decodeURIComponent(location.hash.substr(1));
 		if(!q) throw {message:`usage: ${location.href.replace(/#|$/,'#')}QUERY (v2)`};
 
-		let data = {
-			vivaldi: await(await fetch('vivaldi_x-search-engine.json')).json()
-		};
-
-		for (let i of ['vivaldi','duckduckgo']) if (se[i](q,data[i]) === true) return;
+		for (let i of ['vivaldi','duckduckgo']) if (await searchFnObj[i](q)) break;
 
 
-	} catch (e) {window.console.error(e);window.alert(e.message||'Error ocured, check console')}
+	} catch (e) {console.error(e);alert(e.message||'Error ocured, check console')};
 
 
 
